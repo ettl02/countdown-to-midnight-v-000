d70b24bec4c1b86dfb2b27566fc5e2d6f2c5e836
Emily Wessel <ettl02@gmail.com>
2018-06-18 17:04:05 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown (x)
   x = 12
-  counter -= 1
+  x -= 1
   loop do
     puts "#{x} SECONDS!"
     until x == 0
@@ -15,7 +15,7 @@ def countdown_with_sleep (countdown)
   while x > 0
     puts "#{x} SECONDS!"
     sleep(1)
-    counter -= 1
+    x -= 1
   end
   "HAPPY NEW YEAR!"
 end
